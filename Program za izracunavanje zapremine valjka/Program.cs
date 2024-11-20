5a19f18306fb3fffa552daa885035583e87b9ea0
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-20 10:05:26 +0100

    Commit

---
+++ Program za izracunavanje zapremine valjka/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VolumeCalculator
+{
+    class Cylinder
+    {
+        static void Main(string[] args)
+        {
+            // Definišemo konstante
+            double radius = 10; // Poluprečnik osnove valjka
+            double height = 50; // Visina valjka
+            double pi = 3.14;   // Vrednost konstante Pi
+
+            // Formula za zapreminu valjka: V = π * r^2 * h
+            double volume = pi * Math.Pow(radius, 2) * height;
+
+            // Ispis rezultata
+            Console.WriteLine($"Zapremina valjka sa poluprečnikom {radius} i visinom {height} iznosi: {volume}");
+        }
+    }
+}
+
